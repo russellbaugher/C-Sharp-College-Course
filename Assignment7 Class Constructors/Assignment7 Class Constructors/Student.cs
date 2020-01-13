93601381b5288a3b5f0e4f0102857f933cc41446
Russell Baugher <44611493+russellbaugher@users.noreply.github.com>
2020-01-13 17:35:50 -0500

    Add files via upload

---
+++ Assignment7 Class Constructors/Assignment7 Class Constructors/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7_Class_Constructors
+{
+    public class Student
+    {
+        // Data Members //
+        public static int Count; // Store total # of students
+        private static readonly Random rnd = new Random(); // Randomly assemble student ID
+        private string firstName;
+        private string lastName;
+        private int sID;
+
+        // Public Properties - (Read-Only Auto-Implemented) //
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int StudentID { get; set; }
+
+        // C1 - Parameterized Constructor - Called if (if 3 params provided) //
+        public Student(string first, string last, int id) // Initialize Student
+        {
+            FirstName = first;
+            LastName = last;
+            StudentID = id;
+            ++Count;
+        }
+
+        // C2 - Constructor for Handling Null Arguments (if 0,1,2 params provided) //
+        public Student(string first = "", string last = "")
+        {
+            FirstName = first;
+            LastName = last;
+            StudentID = rnd.Next(1000, 9999);
+            ++Count;
+        }
+    } //End Student
+}
---
+++ Assignment7 Class Constructors/Assignment7 Class Constructors/StudentTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment7_Class_Constructors
+{
+    public class StudentTest
+    {
+        static void Main(string[] args)
+        {
+            // Create Student List
+            List<Student> myList = new List<Student>();
+
+            // Create s1 Object
+            Student s1 = new Student();
+            s1.FirstName = "John";
+            s1.LastName = "Smith";
+            s1.StudentID = 2560;
+            myList.Add(s1);
+
+            // Add Objects to List (s2,s3,s4,s5)
+            Student s2 = new Student("Peter"); // 1 param, call C2
+            myList.Add(s2);
+
+            Student s3 = new Student("Morgan", "Simmons"); // 2 params, call C2
+            myList.Add(s3);
+
+            Student s4 = new Student("James", "Walters"); // 2 params, call C2
+            myList.Add(s4);
+
+            Student s5 = new Student("Linda", "Scott", 1005); // 3 params, call C1
+            myList.Add(s5);
+
+            // Output to User //
+            Console.WriteLine("\nTotal Students: {0}", Student.Count);
+
+            // Iterate Through List & Print //
+            foreach (Student s in myList)
+            {
+                Console.WriteLine("Student Name: {0} {1} \tStudent ID: {2}",                           
+                                  s.FirstName, s.LastName, s.StudentID);
+            }
+        } //End Main
+    } //End StudentTest
+}
