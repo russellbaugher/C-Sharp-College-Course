0f64a95ba01330f2c2442440525706ba3f75c197
Russell Baugher <44611493+russellbaugher@users.noreply.github.com>
2020-01-13 16:01:22 -0500

    Add files via upload

---
+++ Exam1 - Person/Exam1 - Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1_Person
+{
+    public class Person
+    {
+        private string name;
+        public string Name
+        { get; set; }
+
+        private int age;
+        public int Age
+        { get; set; }
+                //if (value >= 65)
+                //Console.WriteLine("Retire!");
+
+        private int yearstowork;
+        public int YearsToWork
+        { get; set; }
+
+        //Subtract Age from 65
+        public void YTK()
+        {
+            int YearsToWork = 65 - Age;
+            Console.WriteLine($"You will work: {YearsToWork} years before you retire.");
+        }
+    }
+        
+}
+
+
+
+
+
+//Initialized variables
+//private string name = "";
+//private int age = 0;
+//private int YearsToWork = 0;
---
+++ Exam1 - Person/Exam1 - Person/PersonTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constructor
+            Person myPerson = new Person();
+
+            // User Input Name
+            Console.WriteLine("Enter Name: ");
+            myPerson.Name = Console.ReadLine();
+
+            //User Input Age
+            Console.WriteLine("Enter Age: ");
+            myPerson.Age = int.Parse(Console.ReadLine());
+
+            //Return # Work Years Left
+            myPerson.YTK();
+        }
+    }
+}
