36dfbf549941bccd137be40c71ef02a66af1e8bd
Russell Baugher <44611493+russellbaugher@users.noreply.github.com>
2020-01-13 16:00:31 -0500

    Add files via upload

---
+++ Assignment2 Calculator1/Assignment2 Calculator1/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment2_Calculator1
+{
+    public class Calculator
+    {
+        //Declare op1
+        private readonly double firstOperand;
+        public double FirstOperand
+        { get; set; }
+
+        //Declare op2
+        private readonly double secondOperand;
+        public double SecondOperand
+        { get; set; }
+
+        //Declare Result
+        private double result;
+        public double Result
+        { get; set; }
+
+        //Add op1 + op2
+        public void Addition()
+        {
+            double Result = FirstOperand + SecondOperand;
+            Console.WriteLine($"Sum: {Result}");
+        }
+    }
+}
---
+++ Assignment2 Calculator1/Assignment2 Calculator1/CalculatorRun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Calculator1
+{
+    class CalculatorRun
+    {
+        static void Main(string[] args)
+        {
+            //Constructor
+            Calculator MyCalc = new Calculator();
+
+            Console.WriteLine("Addition Calculator");
+
+            //User Input Two Operands
+            Console.WriteLine("enter first number: ");
+            MyCalc.FirstOperand = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number: ");
+            MyCalc.SecondOperand = double.Parse(Console.ReadLine());
+
+            //Return Sum
+            MyCalc.Addition();
+        }
+    }
+}
